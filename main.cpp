10ca7b6f5fa23b63771cfd456708ab7f5daf55a4
Eunice Wang <ewang117@xe-01.cs.ucr.edu>
2023-05-19 09:48:31 -0700

    added main.cpp with basic skeleton

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // Backstory
+
+    // encounter dog
+
+    // tutorial fight
+
+    // encounter monkey
+
+    // fight demon 1
+
+    // encounter bird
+    
+    // fight demon 2
+
+    // fight last boss
+
+    return 0;
+}
