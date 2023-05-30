83be286305811eefa19e701e7ea266fcfcdcc0b5
jaeceralde <jaeceralde@yahoo.com>
2023-05-29 17:47:49 -0700

    narrations for the last parts of the story

---
+++ headers/finalNarrations.hpp
@@ -0,0 +1,12 @@
+#ifndef FINALNARRATIONS_HPP
+#define FINALNARRATIONS_HPP
+#include <iostream>
+using namespace std;
+
+class FinalNarrations {
+    public:
+        void meetDemonKing();
+        void epilogue();
+};
+
+#endif
---
+++ source/finalNarrations.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "../headers/finalNarrations.hpp"
+using namespace std;
+
+void FinalNarrations::meetDemonKing() {
+    cout << "YOU: \"Thank you… I don't think I would've survived that one without your help.\"" << endl << endl;
+
+    cout << "You suddenly knelt on the ground, as though your legs could no longer hold your weight. Then, with a mere sigh, you opted to lie down on the ground completely." << endl << endl;
+
+    cout << "DOG: \"Are you alright?\"" << endl << endl;
+
+    cout << "YOU: \"Yeah, just give me a minute. I just need to calm my heart down, it's still beating fast with all the adrenaline from fighting.\"" << endl << endl;
+
+    cout << "BIRD: \"Better calm it down soon, we need to get a move on before another demon shows up.\"" << endl << endl;
+
+    cout << "YOU: \"I know.\"" << endl << endl;
+
+    cout << "You closed your eyes in an effort to make your heart calm down and thought of anything that could bring serenity into your being." << endl
+         << "You thought of your parents, of your friends, of your childhood, of every reason you have why you chose to fight the Demon King in the first place." << endl << endl;
+
+    cout << "And then, once your heart was no longer trying to escape your chest, you stood up with a stronger resolve to finish what you started." << endl << endl;
+
+    cout << "Your team crossed the remaining terrain en route to the Demon King's lair. To better prepare you and your companions for the upcoming battle, you thought of ways to improve" << endl
+         << "your teamwork — which you immediately incorporated into your journey. And, akin to how it would have been in a montage," << endl
+         << "your so-called \"teamwork training\" finished just in time for your team to lure out the Demon King out of his lair." << endl << endl;
+
+    cout << "DEMON KING: \"Who dares to be in proximity of the Demon King?!\"" << endl << endl;
+
+    cout << "MONKEY: \"We are!\"" << endl << endl;
+
+    cout << "DEMON KING: \"Just— just mere mortals? You make me laugh.\"" << endl << endl;
+
+    cout << "YOU: \"Then, in exchange for making you laugh… Would you let us have the honor of taking your life?\"" << endl << endl;
+}
+
+void FinalNarrations::epilogue() {
+    cout << "The Demon King's death also brought upon the disappearance of his minions in this realm, since all their lifelines were connected to his." << endl
+         << "With the eradication of inherently evil beings, the weather appeared to become brighter — as if it had finally healed from its sickness." << endl << endl;
+
+    cout << "You and your companions split the demons' loot amongst yourselves, as both a proof of and a reward for your victory." << endl
+         << "Then, you all parted ways with a small promise of visiting each other once in a while." << endl << endl;
+
+    cout << "You returned home a hero, with your village back to how it once was." << endl << endl;
+
+    cout << "FIN." << endl << endl;
+}
