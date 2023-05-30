01d2ae05eaf69168c76ce3cd30b9296c0f96b8ea
jaeceralde <jaeceralde@yahoo.com>
2023-05-29 17:16:22 -0700

    narrations to be called by battle class directly

---
+++ headers/afterBattles.hpp
@@ -0,0 +1,12 @@
+#ifndef AFTERBATTLES_HPP
+#define AFTERBATTLES_HPP
+#include <iostream>
+using namespace std;
+
+class AfterBattles {
+    public:
+        void afterBattleNarrations(int lvl, bool failedOutcome);
+        void afterFinalBattleNarrations(int lvl, bool failedOutcome);
+};
+
+#endif
