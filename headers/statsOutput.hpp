bbf5c8a70fdb9f7c4538659ab512a2b6f538e0fc
Eunice Wang <ewang117@xe-01.cs.ucr.edu>
2023-06-09 09:26:40 -0700

    updated main and stats

--- headers/statsOutput.hpp
+++ headers/statsOutput.hpp
@@ -9,8 +9,14 @@
 #include "../headers/bird.hpp"
 using namespace std;
 
+extern Momotaro* momotaro;
+extern Dog* dog;
+extern Monkey* monkey;
+extern Bird* bird;
+
 class StatsOutput {
   public:
+    void printStats(int numCompanions);
     void printStats(Momotaro *s_momotaro, Character *s_companion);
     void printStats(Momotaro *momotaro, Dog *dog, Monkey *monkey, Bird *bird);
 };
--- source/statsOutput.cpp
+++ source/statsOutput.cpp
@@ -4,6 +4,59 @@
 
 using namespace std;
 
+void StatsOutput::printStats(int numCompanions) {
+    cout << "=========================== STATS ===========================" << endl;
+    cout << "|MOMOTARO\t|";
+    if (numCompanions >= 1) {
+        cout << "DOG\t\t|";
+    }
+    if (numCompanions >= 2) {
+        cout << "MONKEY\t\t|";
+    }
+    if (numCompanions == 3) {
+        cout << "BIRD";
+    }
+    cout << endl;
+
+    cout << "|HP  : " << momotaro->getHP() << "/" << momotaro->getMaxHP() << "\t|";
+    if (numCompanions >= 1) {
+        cout << "HP  : " << dog->getHP() << "/" << dog->getMaxHP() << "\t|";
+    }
+    if (numCompanions >= 2) {
+        cout << "HP  : " << monkey->getHP() << "/" << monkey->getMaxHP() << "\t|";
+    }
+    if (numCompanions == 3) {
+        cout << "HP  : " << bird->getHP() << "/" << bird->getMaxHP();
+    }
+    cout << endl;
+
+    cout << "|ATK : " << momotaro->getAtk() << "\t|";
+    if (numCompanions >= 1) {
+        cout << "ATK : " << dog->getAtk() << "\t|";
+    }
+    if (numCompanions >= 2) {
+        cout << "ATK : " << monkey->getAtk() << "\t|";
+    }
+    if (numCompanions == 3) {
+        cout << "ATK : " << bird->getAtk() << "\t|";
+    }
+    cout << endl;
+
+    cout << "|DEF : " << momotaro->getDef() << "\t|";
+    if (numCompanions >= 1) {
+        cout << "DEF : " << dog->getDef() << "\t|";
+    }
+    if (numCompanions >= 2) {
+        cout << "DEF : " << monkey->getDef() << "\t|";
+    }
+    if (numCompanions == 3) {
+        cout << "DEF : " << bird->getDef() << "\t|";
+    }
+    cout << endl;
+    cout << "Selected companion: " << momotaro->getAnimal() << endl;
+    cout << "=============================================================" << endl << endl;
+}
+
 void StatsOutput::printStats(Momotaro *s_momotaro, Character *s_companion) {
 cout << "=========================== STATS ===========================" << endl;
     cout << "|MOMOTARO\t|DOG\t\t|" << endl;
